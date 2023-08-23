686d977e0447b9d3b16455a5112edbc73021a449
jayjayswal07 <143029741+jayjayswal07@users.noreply.github.com>
2023-08-24 00:35:52 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,7 @@
+a=int(input("enter value of A:"))
+b=int(input("enter value of B:"))
+print("value before swapping")
+print("A= " ,a ,"and" "B =",b)
+b,a=a,b
+print("values after swapping")
+print("A= " ,a ,"and" "B =",b)
