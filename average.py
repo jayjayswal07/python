9f95648df019003aae82aba46f32a86b600f9e16
jayjayswal07 <143029741+jayjayswal07@users.noreply.github.com>
2023-08-24 00:36:09 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,10 @@
+print("calculating average marks of student of of 500 ")
+sub1=int(input("enter marks in sub1 ="))
+sub2=int(input("enter marks in sub2 ="))
+sub3=int(input("enter marks in sub3 ="))
+sub4=int(input("enter marks in sub4 ="))
+sub5=int(input("enter marks in sub5 ="))
+
+average=(sub1+sub2+sub3+sub4+sub5)/5
+
+print("avearage of a student =",average)
