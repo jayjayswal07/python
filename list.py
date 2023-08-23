9015281ab96112afb56c9f6cf05f8ce6a4d7bca7
jayjayswal07 <143029741+jayjayswal07@users.noreply.github.com>
2023-08-24 00:36:46 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,20 @@
+list1 =[1,2,3,4]
+list2 =["parveen","jay","akhil"]
+list3 =["parveen",1,"jay",2]
+
+
+print(list1)
+print(list2)
+print(list3)
+print(list1[0])
+print(list2[1])
+print(list3[2])
+print(type(list1))
+if 3 in list1:
+    print("yes")
+else:
+        print("no")
+if "par" in "parveen":
+    print("yes")
+else:
+        print("no")
